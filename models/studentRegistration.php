36aee0e0b4c416f540b99b4f2986d52215792946
Mohsin Ghani <Mohsinghani.777@gmail.com>
2016-11-16 09:15:59 -0800

    Add files via upload

---
+++ models/studentRegistration.php
@@ -0,0 +1,16 @@
+<?php
+class StudentRegistration extends CI_Model
+{
+   function __construct()
+   {
+       parent:: __construct();
+   }
+
+   public function stuRegistration($data)
+   {
+       //$this->selectDB->insert('tableName',Data/Info for insert);
+       $this->db->insert('students',$data);
+       return "Successfully Registeration";
+   }
+}
+?>
